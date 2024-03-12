b655976d1af21ba97965d6206290ea5f28edb2a6
GuttenWinnie <sergey.anisimov.1996@mail.ru>
2024-03-13 00:22:04 +0300

    module5-task2

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
 //Функция для проверки длины строки
-const checkLength = (string = '',maxSymbols) => string.length <= maxSymbols;
+const checkLength = (string = ' ',maxSymbols) => string.length <= maxSymbols;
 checkLength();
 
 //Функция для проверки на палиндром
@@ -29,3 +29,24 @@ const exractNum = (string) => {
 };
 
 exractNum('2023 год');
+
+function countTime (startTime, endTime, meetTime, spanMeet) {
+
+  startTime = startTime.split(':');
+  startTime = startTime[0] * 60 + +startTime[1];
+  endTime = endTime.split(':');
+  endTime = endTime[0] * 60 + +endTime[1];
+  meetTime = meetTime.split(':');
+  meetTime = meetTime[0] * 60 + +meetTime[1];
+
+  if (startTime > meetTime || endTime < (meetTime + spanMeet)) {
+    return false;
+  }
+  return true;
+}
+
+console.log(countTime('08:00', '17:30', '14:00', 90));
+console.log(countTime('8:0', '10:0', '8:0', 120));
+console.log(countTime('08:00', '14:30', '14:00', 90));
+console.log(countTime('14:00', '17:30', '08:0', 90));
+console.log(countTime('8:00', '17:30', '08:00', 900));
